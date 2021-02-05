1faece7711a308be8d7492c0db1b2fdabf319468
Denis <dennisouma38@gmail.com>
2021-02-05 11:18:16 +0300

    filtering and Delete function js finished

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ const taskForm = document.querySelector('.task-form');
 const clearBtn =   document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskList = document.querySelector('.collection');
-const taskInput = document.querySelector('#task');
-
+const taskInput = document.querySelector('#task'); 
 
 //submit event
 taskForm.addEventListener('submit',function(e) {
     if(taskInput.value === ''){
         alert('Please enter a Task!')
+        return  
     }
     const inputLi = document.createElement('li');
     inputLi.className = 'collection-items';
@@ -20,14 +20,24 @@ taskForm.addEventListener('submit',function(e) {
     deleteLink.innerHTML = '<i class = "fa fa-remove"></i>'
     inputLi.appendChild(deleteLink);
     taskList.appendChild(inputLi);
+    taskInput.value = ''; 
     // clearing the Inputed
-    taskInput.value = '';   
+    const listItem = document.querySelectorAll('i');
+    listItem.forEach(listItem=>{
+        listItem.addEventListener('click',e=>{
+            e.target.parentNode.parentNode.remove()
+        })
+    })
     e.preventDefault()
 })
 
+
+
 clearBtn.addEventListener('click', function(){
-    const list = document.querySelectorAll('.task-input')
-    console.log(list[0].textContent)
+    const list = document.querySelectorAll('li')
+    console.log(list.value)
+    
+
     for(var i=0; i<list.length; i++){
         if(list[i].textContent === filter.value){
             list[i].remove();
